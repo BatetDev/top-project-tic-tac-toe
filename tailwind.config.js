8dee7c462b698e90ce85ba0f64ba69505dac7fde
fr4nbtt <batet.francisco@gmail.com>
2025-02-07 10:23:47 -0300

    feat: Add Orbitron font, minor style tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        orbitron: ["Orbitron", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
